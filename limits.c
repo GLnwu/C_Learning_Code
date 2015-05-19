828933247768b3f600b7f81c50e4162d1b94259b
GLnwu <bingtang1021@gmail.com>
2015-05-19 11:40:30 +0800

    Delete the rendundancy files

--- limits.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<limits.h>
-/* 本源代码列出一些limits.h头文件内的符号常量的值 */
-int main(void)
-{
-	printf("CHAR_BIT( 一个char的位数),%d\n",CHAR_BIT);
-	printf("CHAR_MAX(char类型的最大值),%d\n",CHAR_MAX);
-	return 0;
-}
