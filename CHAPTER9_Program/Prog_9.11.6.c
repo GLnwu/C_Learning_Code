cc5ced0aadfe12ee65542672101bfd19143f73b6
GLnwu <bingtang1021@gmail.com>
2015-06-09 20:01:58 +0800

    revise Prog_9.11.6.c

--- CHAPTER9_Program/Prog_9.11.6.c
+++ CHAPTER9_Program/Prog_9.11.6.c
@@ -1,16 +1,25 @@
-#include<stdio.>
+#include<stdio.h>
 #include<ctype.h>
-
+void Process(char );
 int Judge(char);
 
 int main(void)
 {
 	char c;
+	int d;
 
 	while((c = getchar()) != EOF)
 	{
-		if(Judge(c));
+		d = Judge(c);
+		if(d != -1)
+			printf("%c =  %d  ",c,d);
+	}
+
+	return 0;
+}
+
 int Judge(char c)
+/* 判断一个给定的字符是否为字母，是则返回 */
 {	
 	int d;
 
@@ -18,6 +27,6 @@ int Judge(char c)
 		d = tolower(c)-96;
 	else
 		d = -1;
+
 	return d;
 }
-
