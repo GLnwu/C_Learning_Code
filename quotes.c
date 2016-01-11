bc0cc712d916f6533c76e18dcc1350381bbb146b
GLnwu <bingtang1021@gmail.com>
2016-01-11 17:10:17 +0800

    add quotes.c to the master branch

---
+++ quotes.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: quotes.c
+    > Author: GaoLi
+    > Mail: bingtang1021@gmail.com 
+    > Created Time: 2016年01月11日 星期一 16时46分04秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main(void)
+{
+	printf("%s, %p, %c\n","We","are","space farers");
+	return 0;
+}
