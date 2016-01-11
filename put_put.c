06df028faff140b95000d29c410a9a0c4eb9316d
GLnwu <bingtang1021@gmail.com>
2016-01-12 00:59:07 +0800

    remove put_out.c,add put_put.c

---
+++ put_put.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: put_put.c
+    > Author: GaoLi
+    > Mail: bingtang1021@gmail.com 
+    > Created Time: 2016年01月12日 星期二 00时35分50秒
+ ************************************************************************/
+
+#include<stdio.h>
+void put1(const char *);
+int  put2(const char *);
+
+int main(void)
+{
+	put1(If I'd')
+}
