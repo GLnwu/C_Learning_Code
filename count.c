46a9ca7f4f0a5fed77389efb459c5356d9facf31
GLnwu <bingtang1021@gmail.com>
2016-02-18 20:41:59 +0800

    remove swap file of Prog12.11.5.c , add addadowrd.c count.c and reducto.c

---
+++ count.c
@@ -0,0 +1,40 @@
+/*************************************************************************
+    > File Name: count.c
+    > Author: GaoLi
+    > Mail: bingtang1021@gmail.com 
+    > Created Time: 2016年02月18日 星期四 16时52分18秒
+ ************************************************************************/
+/* count.c -- 使用标准 I/O */
+#include<stdio.h>
+
+#include<stdlib.h>	//ANSI C的exit()原型
+int main(int argc,char * argv[])
+{
+	int ch;		//读取时存储每个字符的位置
+	FILE *fp;	//文件指针
+	long count = 0;
+
+	if(argc != 2)
+	{
+		printf("Usage:%s filename\n",argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	if((fp = fopen(argv[1],"r")) == NULL)
+	{
+		printf("Can't open %s\n",argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while((ch = getc(fp)) != EOF)
+	{
+		putc(ch,stdout);	//相当于putchar(ch)
+		count++;
+	}
+	if(fclose(fp) != 0)
+	{
+		printf("Error in closing file %s\n",argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	printf("File %s has %ld chararcters\n",argv[1],count);
+
+	return 0;
+}
